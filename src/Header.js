7719d48a3740ef213ee09424a20070b2c015624c
Gabriel Adrián Romero <gabrieladrianromero28@gmail.com>
2020-09-09 18:23:28 -0300

    Added BasketCount to the Header

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,11 @@ import "./Header.css";
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+  const [ { basket }, dispatch] = useStateValue();
+
   return (
     <div className="header">
       <Link to="/">
@@ -40,7 +43,7 @@ function Header() {
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
             <span className="header__optionLineTwo header__basketCount">
-              0
+              {basket?.length}
             </span>
           </div>
         </Link>
