1efa5e89c24740b385089451383cc031a97b2220
Gabriel Adrián Romero <gabrieladrianromero28@gmail.com>
2020-09-09 17:46:57 -0300

    Added Reducer

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,24 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
-function Product({ title, image, price, rating }) {
+
+function Product({id, title, image, price, rating }) {
+        const [{ basket }, dispatch] = useStateValue();
+
+        const addToBasket = () => {
+            // dispatch the item into the data layer
+            dispatch({
+                type: 'ADD_TO_BASKET',
+                item: {
+                    id: id,
+                    title: title,
+                    image: image,
+                    price: price,
+                    rating: rating,
+                },
+            });
+        }
     return (
         <div className="product">
             <div className="product__info">
@@ -12,14 +29,16 @@ function Product({ title, image, price, rating }) {
                 </p>
                 
                 <div className="product__rating">
-                    {Array(rating).fill().map((_,i) => (
-                        <p>⭐</p>
+                    {Array(rating)
+                        .fill()
+                        .map((_,i) => (
+                            <p>⭐</p>
                     ))}
                 </div>
             </div>
 
             <img src={image} alt="" />
-            <button>Add to basket</button>
+            <button onClick={addToBasket}>Add to basket</button>
         </div>
     );
 }
