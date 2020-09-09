0348fcda8db77eafa35fc39967e1252396db115c
Gabriel Adrián Romero <gabrieladrianromero28@gmail.com>
2020-09-09 14:40:15 -0300

    Added Ckeckout Page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
     //BEM
     <Router>
       <div className="app">
+
         <Switch>
           <Route path="/checkout">
             <Header />
+            <Checkout />
           </Route>
 
           <Route path="/">
